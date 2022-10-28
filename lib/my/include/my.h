e0f7c626552c52083b52dc7625755cd8419f4ee7
Corentin COUTRET-ROZET <corentin.rozet@epitech.eu>
2022-10-28 11:27:52 +0200

    [ADD] - base for library

---
+++ lib/my/include/my.h
@@ -0,0 +1,17 @@
+/*
+** Description:
+** Library's Prototypes
+**
+** Corentin COUTRET-ROZET
+** https://github.com/sheiiva/DuckHunt
+**
+*/
+
+#ifndef MY_H_
+    #define MY_H_
+
+    #include <unistd.h>
+
+    ssize_t my_putchar(char c, int fd);
+
+#endif /* !MY_H_ */
