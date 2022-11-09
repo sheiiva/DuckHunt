9e044ed00c4c638fab1c45472afa8be706f57eb2
Corentin COUTRET-ROZET <corentin.rozet@epitech.eu>
2022-11-09 12:21:18 +0100

    [ADD] - Raising in case of window creation failure

--- sources/graphical/window/window.c
+++ sources/graphical/window/window.c
@@ -8,6 +8,8 @@
 */
 
 #include "new.h"
+#include "raise.h"
+
 #include "window.h"
 
 typedef struct s_class {
@@ -27,6 +29,8 @@ static void Window_ctor(WindowClass *this, va_list *args)
     // Initialize internal resources
     this->mode = (sfVideoMode){1280, 800, 32};
     this->window = sfRenderWindow_create(this->mode, "Duck Hunt", sfResize | sfClose, NULL);
+    if (!this->window)
+        raise("Cannot create any window.");
 
     sfRenderWindow_setMouseCursorVisible(this->window, sfFalse);
     sfRenderWindow_setFramerateLimit(this->window, FRAMERATE);
